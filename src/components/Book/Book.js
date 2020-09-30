3c4f861471601f77726d8632a2dff3aed92fba16
zabir1998 <zabirahmed1998@gmail.com>
2020-09-30 13:13:24 +0600

    mongo done

---
+++ src/components/Book/Book.js
@@ -0,0 +1,92 @@
+import React, { useContext, useState } from 'react';
+import { Link, useParams } from 'react-router-dom';
+import { UserContext } from '../../App';
+import Grid from '@material-ui/core/Grid';
+import DateFnsUtils from '@date-io/date-fns';
+import {
+  MuiPickersUtilsProvider,
+  KeyboardDatePicker,
+} from '@material-ui/pickers';
+import { Button } from '@material-ui/core';
+import Bookings from '../Bookings/Bookings';
+
+const Book = () => {
+    const {bedType} = useParams();
+    const [loggedInUser, setLoggedInUser]= useContext(UserContext);
+    const [selectedDate, setSelectedDate] =useState({
+        checkIn: new Date(),
+        checkOut:new Date()
+    });
+
+    const handleCheckInDate = (date) => {
+        const newDates={...selectedDate}
+        newDates.checkIn=date;
+      setSelectedDate(newDates);
+    };
+
+    const handleCheckOutDate = (date) => {
+        const newDates={...selectedDate}
+        newDates.checkOut=date;
+      setSelectedDate(newDates);
+    };
+
+    const handleBooking=()=>{
+      const newBooking={...loggedInUser,...selectedDate};
+      fetch('http://localhost:5000/addBooking',{
+        method:'POST',
+        headers: {'Content-Type':'application/json'},
+        body:JSON.stringify(newBooking)
+      })
+
+      .then(res=>res.json())
+      .then(data=>{
+        console.log(data);
+      })
+        
+    }
+
+    return (
+        <div style={{textAlign: 'center'}}>
+            <h1>Hello {loggedInUser.name}!Let's book a {bedType} Room.</h1>
+            <p>Want a <Link to="/home">different room?</Link> </p>
+
+            <MuiPickersUtilsProvider utils={DateFnsUtils}>
+      <Grid container justify="space-around">
+        <KeyboardDatePicker
+          disableToolbar
+          variant="inline"
+          format="MM/dd/yyyy"
+          margin="normal"
+          id="date-picker-inline"
+          label="Check in Date"
+          value={selectedDate.checkIn}
+          onChange={handleCheckInDate}
+          KeyboardButtonProps={{
+            'aria-label': 'change date',
+          }}
+        />
+        <KeyboardDatePicker
+          margin="normal"
+          id="date-picker-dialog"
+          label="Check out Date"
+          format="MM/dd/yyyy"
+          value={selectedDate.checkOut}
+          onChange={handleCheckOutDate}
+          KeyboardButtonProps={{
+            'aria-label': 'change date',
+          }}
+        />
+       
+      </Grid>
+      <Button onClick={handleBooking} variant="contained" color="primary">
+  Book Now
+</Button>
+    </MuiPickersUtilsProvider>
+
+    <Bookings></Bookings>
+
+        </div>
+    );
+};
+
+export default Book;
---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCdI2QjpMQhlhWthj2ZmjtVqeH4jvUXR3Q",
+  authDomain: "burj-al-arab-mongo.firebaseapp.com",
+  databaseURL: "https://burj-al-arab-mongo.firebaseio.com",
+  projectId: "burj-al-arab-mongo",
+  storageBucket: "burj-al-arab-mongo.appspot.com",
+  messagingSenderId: "851143744869",
+  appId: "1:851143744869:web:e24bc8180e17ca129d958c"
+};
+
+export default firebaseConfig;
